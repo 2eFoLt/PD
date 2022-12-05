ea7c05d3422a79ca54038816f56898e90adf3eef
2eFoLt <grig2205@gmail.com>
2022-12-05 15:55:54 +0300

    html-fix

--- main.py
+++ main.py
@@ -32,48 +32,56 @@ def pr():
 @app.route('/mk', methods=['GET'])
 def mk():
     return render_template('mk.html')
-# @app.route('/checkout', methods=['GET'])
-# def checkout():
-#     return render_template('newCheckoutPage.html')
-#
-#
-# @app.route('/login')
-# def login():
-#     if 'email' in session:
-#         return redirect(url_for('main'))
-#     return render_template('newLoginPage.html')
-#
-#
-# @app.route('/main')
-# def main():
-#     if 'email' in session:
-#         return render_template('newMainPage.html', login=session['email'])
-#     else:
-#         redirect(url_for('login'))
-#
-#
-# @app.route('/logout')
-# def logout():
-#     session.pop('email', None)
-#     session.pop('hash', None)
-#     session.pop('id', None)
-#     return redirect(url_for('index'))
-#
-#
-# @app.route('/log-success', methods=['POST'])
-# def success():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         passwd = request.form['password']
-#         userid = random.randint(1, 1000)
-#         if email not in session:
-#             session['email'] = email
-#             session['hash'] = generate_password_hash(passwd)
-#             session['id'] = userid
-#         return redirect(url_for('main'))
-#     else:
-#         pass
 
+
+if __name__ == '__main__':
+    app.run(
+        # host='0.0.0.0', port=5151
+    )
+
+"""
+@app.route('/checkout', methods=['GET'])
+def checkout():
+    return render_template('newCheckoutPage.html')
+
+
+@app.route('/login')
+def login():
+    if 'email' in session:
+        return redirect(url_for('main'))
+    return render_template('newLoginPage.html')
+
+
+@app.route('/main')
+def main():
+    if 'email' in session:
+        return render_template('newMainPage.html', login=session['email'])
+    else:
+        redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    session.pop('hash', None)
+    session.pop('id', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/log-success', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form['email']
+        passwd = request.form['password']
+        userid = random.randint(1, 1000)
+        if email not in session:
+            session['email'] = email
+            session['hash'] = generate_password_hash(passwd)
+            session['id'] = userid
+        return redirect(url_for('main'))
+    else:
+        pass
+"""
 
 """
 Route template:
@@ -81,7 +89,3 @@ Route template:
 def <html-name>():
     return render_template('html-file')
 """
-if __name__ == '__main__':
-    app.run(
-         # host='0.0.0.0', port=5151
-    )
